a86ecc8a3f9bd183c089c5ec7fa1c99cbcaf8cc9
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2022-01-23 21:25:44 -0800

    Parse each FF team's unique ID

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import os
 from bs4 import BeautifulSoup
 import requests
+import re
 
+attr_team_id = re.compile("teamId-[0-9]")
 
 NFL_FF_LEAGUE_PREFIX = "https://fantasy.nfl.com/league"
 NFL_FF_LEAGUE_ID = os.environ["NFL_FF_LEAGUE_ID"]
 NFL_FF_LEAGUE_URL = f'{NFL_FF_LEAGUE_PREFIX}/{NFL_FF_LEAGUE_ID}'
 
-res = requests.get(NFL_FF_LEAGUE_URL)
-res.raise_for_status()
+TEAM_ID_PREFIX = "teamId-"
 
-soup = BeautifulSoup(res.content, "html.parser")
-print(soup.prettify)
+def parse_team_id(span):
+    for val in span.attrs["class"]:
+        if val.startswith(TEAM_ID_PREFIX):
+            return val.strip(TEAM_ID_PREFIX)
+
+def main():
+    res = requests.get(NFL_FF_LEAGUE_URL)
+    res.raise_for_status()
+
+    soup = BeautifulSoup(res.content, "html.parser")
+    span_tags = soup.find_all("span", attrs={"class": attr_team_id})
+
+    team_ids = set(parse_team_id(span) for span in span_tags)
+    print(team_ids)
+
+if __name__ == "__main__":
+    main()
