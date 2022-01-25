1bd7901e58c892ef4fb223d4d9c7e4414ccc97dc
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2022-01-25 15:28:52 -0800

    Refactor implementation for saving/dumping Teams data

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from dataclasses import dataclass, asdict
+from dataclasses import asdict
 import os
-from bs4 import BeautifulSoup
-import requests
 import re
-import json
+import repo
+import requests
+
+from bs4 import BeautifulSoup
+from models import Team, Teams
 
 attr_team_id = re.compile("teamId-[0-9]")
 
@@ -13,50 +15,11 @@ NFL_FF_TEAM_URL = f"{NFL_FF_LEAGUE_URL}/team"
 
 TEAM_ID_PREFIX = "teamId-"
 
-@dataclass
-class Team:
-    """Class for teams in an NFL FF league"""
-    id: int
-    name: str = ""
-    owner: str = ""
-
-def parse_team_id(span):
-    for val in span.attrs["class"]:
-        if val.startswith(TEAM_ID_PREFIX):
-            return val.strip(TEAM_ID_PREFIX)
+TEAMS_JSON = "teams.json"
+NFL_FF_GAMECENTER_URL = f"{NFL_FF_TEAM_URL}/" + "{team_id}/gamecenter?week={week}"
 
 
-def initialize_teams(team_ids):
-    teams = {team_id: Team(team_id) for team_id in team_ids}
-
-    for team_id in teams:
-        res = requests.get(NFL_FF_TEAM_URL + f"/{team_id}")
-        res.raise_for_status()
-
-        soup = BeautifulSoup(res.content, "html.parser")
-
-        span = soup.find("span", attrs={"class": "selecter-item", "data-value": team_id})
-        if span is None:
-            raise Exception(f"failed to find span tag containing the team name for team id: {team_id}")
-        teams[team_id].name = span.get_text().strip().lower()
-
-        anchor = soup.find("a", attrs={"class": "userName"})
-        if anchor is None:
-            raise Exception(f"failed to find anchor tag containing the team owner name for team id: {team_id}")
-        teams[team_id].owner = anchor.get_text().strip().lower()
-
-    return teams
-
-def save_as_json(teams):
-    payload = {"teams": []}
-    
-    for _, team in teams.items():
-        payload["teams"].append(asdict(team))
-
-    with open("payload.json", "w") as f:
-        json.dump(payload, f, indent=4, sort_keys=True)
-
-def main():
+def fetch_teams():
     res = requests.get(NFL_FF_LEAGUE_URL)
     res.raise_for_status()
 
@@ -64,8 +27,50 @@ def main():
     span_tags = soup.find_all("span", attrs={"class": attr_team_id})
 
     team_ids = set(parse_team_id(span) for span in span_tags)
-    teams = initialize_teams(team_ids)
-    save_as_json(teams)
+    teams = []
+
+    for team_id in team_ids:
+        res = requests.get(NFL_FF_TEAM_URL + f"/{team_id}")
+        res.raise_for_status()
+
+        soup = BeautifulSoup(res.content, "html.parser")
+
+        span = soup.find(
+            "span", attrs={"class": "selecter-item", "data-value": team_id}
+        )
+        if span is None:
+            raise Exception(
+                f"failed to find span tag containing the team name for team id: {team_id}"
+            )
+        name = span.get_text().strip().lower()
+
+        anchor = soup.find("a", attrs={"class": "userName"})
+        if anchor is None:
+            raise Exception(
+                f"failed to find anchor tag containing the team owner name for team id: {team_id}"
+            )
+        owner = anchor.get_text().strip().lower()
+        teams.append(Team(team_id, name, owner))
+
+    return Teams(teams)
+
+
+def parse_team_id(span):
+    for val in span.attrs["class"]:
+        if val.startswith(TEAM_ID_PREFIX):
+            return int(val.strip(TEAM_ID_PREFIX))
+
+
+def main():
+    if repo.exists(TEAMS_JSON):
+        print(f"reading {TEAMS_JSON}")
+        teams = repo.read(TEAMS_JSON, Teams)
+    else:
+        print(f"sending requests to NFL to fetch team data")
+        teams = fetch_teams()
+        repo.commit(teams, TEAMS_JSON)
+
+    print(repo.read(TEAMS_JSON, Teams))
 
 
 if __name__ == "__main__":
