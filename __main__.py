e7cf851a95727a5e717d60eb2d067e12a4fb5a5f
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2022-02-09 17:43:51 -0800

    Implement DBClient for airtable

--- __main__.py
+++ __main__.py
@@ -1,11 +1,13 @@
 import os
 
-from nflfantaspy.db.backends import json
+from nflfantaspy.db.backends import airtable
 from nflfantaspy.nfl import fantasy
-from nflfantaspy.spyder import schedule
+from nflfantaspy.spyder import schedule as sch
 from nflfantaspy.parser import schedule, settings
 
 NFL_FF_LEAGUE_ID = os.environ["NFL_FF_LEAGUE_ID"]
+AIRTABLE_API_KEY = os.environ["AIRTABLE_API_KEY"]
+AIRTABLE_BASE_ID = os.environ["AIRTABLE_BASE_ID"]
 # SEASON_YEARS = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
 
 
@@ -15,9 +17,14 @@ def main():
     parser = settings.Parser(res.content)
     context = parser.parse()
 
-    spyder = schedule.Spyder(
-        nfl, schedule.Parser, json.DatabaseClient("games.json"), context
-    )
+    cfg = {
+        "api_key": AIRTABLE_API_KEY,
+        "base_id": AIRTABLE_BASE_ID,
+        "table_name": "Season 2021",
+    }
+    db = airtable.DatabaseClient(cfg)
+
+    spyder = sch.Spyder(nfl, schedule.Parser, db, context)
     spyder.execute()
 
 
--- nflfantaspy/db/backends/airtable.py
+++ nflfantaspy/db/backends/airtable.py
@@ -1,8 +1,30 @@
-from nflfantaspy.db.backends.base import BaseDatabaseClient
-
-
+from pyairtable.api import Table
 from nflfantaspy.db.backends.base import BaseDatabaseClient
 
 
 class DatabaseClient(BaseDatabaseClient):
-    pass
+    def __init__(self, cfg: dict):
+        self.cfg = cfg
+        self.table = Table(cfg["api_key"], cfg["base_id"], cfg["table_name"])
+
+    def save(self, payload):
+        self.destructive_reset()
+
+        for i, game in enumerate(payload):
+            record = {
+                "ID": i,
+                "Week": game["week"],
+                "Home Team": game["home_id"],
+                "Away Team": game["away_id"],
+                "Home Team Score": game["home_score"],
+                "Away Team Score": game["away_score"],
+                "Game Type": "REGULAR",
+            }
+            self.table.create(record)
+
+    def destructive_reset(self):
+        ids = []
+        for games in self.table.iterate():
+            for game in games:
+                ids.append(game["id"])
+        self.table.batch_delete(ids)
