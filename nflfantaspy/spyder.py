7ecd7aa9b00123d018847db4ef526a3c07af5042
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2022-02-12 23:27:26 -0800

    Add argparse as replacement for reading environment variables

--- nflfantaspy/spyder.py
+++ nflfantaspy/spyder.py
@@ -1,20 +1,21 @@
 from typing import Type
 
-from nflfantaspy.settings import NFL_FF_LEAGUE_ID
 from nflfantaspy.parser import Parser
 
 
 class FantasyHistory:
-    BASE_URL = f"https://fantasy.nfl.com/league/{NFL_FF_LEAGUE_ID}/history"
-
-    def __init__(self, fetch_fn, parser_class: Type[Parser], content_type: str):
+    def __init__(
+        self, league_id: int, fetch_fn, parser_class: Type[Parser], content_type: str
+    ):
+        self.league_id = league_id
+        self.base_url = f"https://fantasy.nfl.com/league/{league_id}/history"
         self.fetch_fn = fetch_fn
         self.parser_class = parser_class
         self.content_type = content_type
 
     def execute(self, **kwargs):
         year = kwargs["year"]
-        url = f"{self.BASE_URL}/{year}/{self.content_type}"
+        url = f"{self.base_url}/{year}/{self.content_type}"
         content = self.fetch_fn(url, params=self._get_params(**kwargs))
         parser = self.parser_class(content)
         return parser.parse()
@@ -24,13 +25,13 @@ class FantasyHistory:
 
 
 class Teams(FantasyHistory):
-    def __init__(self, fetch_fn, parser_class):
-        super().__init__(fetch_fn, parser_class, "owners")
+    def __init__(self, league_id: int, fetch_fn, parser_class: Type[Parser]):
+        super().__init__(league_id, fetch_fn, parser_class, "owners")
 
 
 class Schedule(FantasyHistory):
-    def __init__(self, fetch_fn, parser_class):
-        super().__init__(fetch_fn, parser_class, "schedule")
+    def __init__(self, league_id: int, fetch_fn, parser_class: Type[Parser]):
+        super().__init__(league_id, fetch_fn, parser_class, "schedule")
 
     def execute(self, year: int):
         page = 1
@@ -47,7 +48,7 @@ class Schedule(FantasyHistory):
     def _get_params(self, year: int, page: int):
         return {
             "gameSeason": year,
-            "leagueId": NFL_FF_LEAGUE_ID,
+            "leagueId": self.league_id,
             "scheduleDetail": page,
             "scheduleType": "week",
             "standingsTab": "schedule",
